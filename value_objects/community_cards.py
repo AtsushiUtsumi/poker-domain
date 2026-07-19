34e69c945542d3be0f8d0d7a119b47cfb3069738
AtsushiUtsumi <safdgx252@gmail.com>
2026-07-19 15:01:08 +0900

    CommunityCardsクラスにHoleCardsオブジェクトを渡して「現状の役」を返すメソッドを実装

--- value_objects/community_cards.py
+++ value_objects/community_cards.py
@@ -1,7 +1,18 @@
 from poker_domain.value_objects.card import Card
+from poker_domain.value_objects.hand import Hand
+from poker_domain.value_objects.hole_cards import HoleCards
 
 
 class CommunityCards(tuple[Card, ...]):
     """ボード(コミュニティカード)。tuple[Card, ...] のサブクラスで、挙動は tuple と完全互換"""
 
     __slots__ = ()
+
+    def evaluate(self, hole_cards: HoleCards) -> Hand:
+        """手札と合わせた現状の役を返す (合計5枚未満の場合は評価不可)"""
+        from poker_domain.hand_evaluator import HandEvaluator
+
+        combined = tuple(self) + tuple(hole_cards)
+        if len(combined) < 5:
+            raise ValueError("役を判定するには合計5枚以上のカードが必要です")
+        return HandEvaluator.evaluate(combined)
